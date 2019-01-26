91610e960a9921268f0c4e790f2bc22db656f013
Gabriel R <gabriel_ribeiro@id.uff.br>
2019-01-26 20:26:05 -0200

    Nova movimentação para o shooter top down

---
+++ Assets/Scripts/Game 2/MouseAiming.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAiming : MonoBehaviour
+{ 
+    Vector3 lookPos;
+    Vector3 lookDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+
+        lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
+    }
+}
