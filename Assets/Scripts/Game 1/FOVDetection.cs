e3669d54a96a77f1b33ae4870b49350716db51ce
Gabriel R <gabriel_ribeiro@id.uff.br>
2019-01-26 04:39:11 -0200

    Script FOV terminado

---
+++ Assets/Scripts/Game 1/FOVDetection.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVDetection : MonoBehaviour
+{
+    public Transform player;
+    public float maxAngle;
+    public float maxRadius;
+
+    private bool isInFov = false;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
+
+        Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, fovLine1);
+        Gizmos.DrawRay(transform.position, fovLine2);
+    
+        if(isInFov)
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+            
+        Gizmos.DrawRay(transform.position, (player.position - transform.position).normalized * maxRadius);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
+    }
+
+    public bool InFOV(Transform target)
+    {
+        Collider[] overlaps = new Collider[10];
+        int count = Physics.OverlapSphereNonAlloc(transform.position, maxRadius, overlaps);
+
+        if (Vector3.Distance(player.position,target.position) < maxRadius)
+        {
+            Vector3 directionBetween = (target.position - transform.position).normalized;
+            directionBetween.y *= 0;
+
+            float angle = Vector3.Angle(transform.forward, directionBetween);
+
+            if (angle <= maxAngle)
+            {
+                Ray ray = new Ray(transform.position, target.position - transform.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, maxRadius))
+                {
+                    if (hit.transform == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isInFov = InFOV(player);
+    }
+}
