7c0613e092d6ca7ee72f2e25ca4366d34622d7a1
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2019-01-26 21:55:44 -0200

    COMMITOU CHATUBA HEIN

---
+++ Assets/Scripts/Game 1/Enemy.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    public float patrolTime;
+    public float seconds;
+    public float lookSpeed;
+
+
+    private Vector3 currentPos;
+    private Vector3 prevPos;
+    private bool isGoing; // is going = true = subindo, is going = false = descendo
+    private int currentWayPoint = 0;
+
+    private void Start()
+    {
+        int randomtWayPoint = Random.Range(0, wayPoints.Length);
+        currentWayPoint = randomtWayPoint;
+        transform.position = wayPoints[currentWayPoint].position;
+        gameObject.SetActive(true);
+
+        StartCoroutine(MoveTroughWayPoints());
+    }
+
+    IEnumerator MoveTroughWayPoints() 
+    {
+        yield return new WaitForSeconds(patrolTime);
+
+        StartCoroutine(Move(wayPoints[currentWayPoint].position));
+
+        yield return new WaitForSeconds(seconds);
+
+        if (currentWayPoint == wayPoints.Length - 1) 
+        {
+            isGoing = false;
+            currentWayPoint--;
+        }
+        else if (currentWayPoint < wayPoints.Length - 1 && currentWayPoint > 0) 
+        {
+            if (isGoing == false)
+            {
+                currentWayPoint--;
+            }
+            else 
+            {
+                currentWayPoint++;
+            }
+        }
+        else if (currentWayPoint == 0) 
+        {
+            isGoing = true;
+            currentWayPoint++;
+        }
+        StartCoroutine(MoveTroughWayPoints());
+    }
+
+    IEnumerator Move(Vector3 nextPosition)
+    {
+        float elapsedTime = 0f;
+
+        Vector3 originalPos = transform.position;
+
+        while (elapsedTime < seconds) 
+        { 
+            transform.position = Vector3.Lerp(originalPos, nextPosition, elapsedTime/seconds);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    /*private void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - prevPos), Time.fixedDeltaTime * lookSpeed);
+    }*/
+}
---
+++ Assets/Scripts/Game 1/PlayerInventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public GameObject warningPanel;
+
+    private bool hasKey;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Key")
+            hasKey = true;
+        else if (other.tag == "Room") 
+        { 
+            if (hasKey == false) 
+            {
+                warningPanel.SetActive(true);
+            }
+            else 
+            { 
+                // Animacao de vitoria.
+            }
+        }
+    }
+}
