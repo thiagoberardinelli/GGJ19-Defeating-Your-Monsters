4784dd94e30c8c5ac261870b178881cf2f409a78
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2019-01-27 19:09:26 -0200

    agora foi

---
+++ Assets/Scripts/Game 2/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text timer;
+    float timeLeft = 50f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.text = Mathf.Round(timeLeft).ToString();
+        if (timeLeft < 0)
+        {
+            CurrentLevel.indexLevel = 3;
+        }
+    }
+}
---
+++ Assets/Scripts/General/HomeCaller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeCaller : MonoBehaviour
+{
+    public void GoToHome() 
+    {
+        LevelManager.instance.LoadLevel("Chat");
+    }
+}
