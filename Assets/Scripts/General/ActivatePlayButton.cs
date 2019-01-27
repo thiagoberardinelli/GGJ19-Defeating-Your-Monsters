dcd876ca9455c71fa8efeb9f980819ba3ee8deda
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2019-01-27 16:52:25 -0200

    animacoa de entrada no jogo e botao de play inicial funcional

---
+++ Assets/Scripts/General/ActivatePlayButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ActivatePlayButton : MonoBehaviour
+{
+    public GameObject playButton;
+    private LevelManager lvlManager;
+
+    private void Start()
+    {
+        lvlManager = FindObjectOfType<LevelManager>();
+    }
+
+    public void ActivateButton() 
+    {
+        playButton.SetActive(true);
+    }
+
+    public void PlayCurrentLevel() 
+    {
+        lvlManager.CurrentLevelToGo();
+    }
+}
---
+++ Assets/Scripts/General/PlayButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+
+    public Animator animator;
+    private LevelManager lvlManager;
+
+    private void Start()
+    {
+        lvlManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        animator.Play("EntryGame");
+    }
+}
