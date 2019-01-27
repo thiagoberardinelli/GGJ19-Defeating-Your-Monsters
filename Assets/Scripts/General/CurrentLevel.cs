7fc37c2b6c02fabc019f7a77d42f4ead1eb0836b
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2019-01-27 15:57:05 -0200

    mensagem na parede na animacao da cena de home mais algumas pequenas correcoes e adicao do basics do TMPro

--- Assets/Scripts/General/CurrentLevel.cs
+++ Assets/Scripts/General/CurrentLevel.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
 public class CurrentLevel : MonoBehaviour
 {
+    public TextMeshProUGUI tmPro;
+
     [HideInInspector] public static int indexLevel = 1;
+
+    private void OnEnable()
+    {
+        if (indexLevel == 1)
+            tmPro.text = "Week 1";
+        else if (indexLevel == 2)
+            tmPro.text = "Week 2";
+        else if (indexLevel == 3)
+            tmPro.text = "Week 3";
+    }
 }
