8fc19aa2c2752457ce64efe53942d15e668b09c2
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2019-01-27 15:34:34 -0200

    retirada do panel de fim do runner e novas animacoes da camera da home

--- Assets/Scripts/Game 3/FinishLine.cs
+++ Assets/Scripts/Game 3/FinishLine.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    public GameObject endPanel;
     public Color endLineColor;
 
     private void OnTriggerEnter(Collider other)
@@ -14,12 +13,6 @@ public class FinishLine : MonoBehaviour
             other.GetComponent<Player>().canMove = false;
             Camera.main.GetComponent<CameraFollow>().StopFollow();
             Camera.main.backgroundColor = endLineColor;
-            Invoke("EndText", 2.5f);
         }
-    }
-
-    void EndText() 
-    {
-        endPanel.SetActive(true);
-    }
+    }  
 }
---
+++ Assets/Scripts/General/CurrentLevel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentLevel : MonoBehaviour
+{
+    [HideInInspector] public static int indexLevel = 1;
+}
