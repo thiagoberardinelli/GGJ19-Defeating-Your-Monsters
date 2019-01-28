29963361db47c3523d7759aa9646bb5b1120e009
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2019-01-28 20:15:52 -0200

    fade out na tela home na entrada da mesma

--- Assets/Scripts/General/ActivatePlayButton.cs
+++ Assets/Scripts/General/ActivatePlayButton.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class ActivatePlayButton : MonoBehaviour
 {
-    public GameObject playButton; 
-
+    public GameObject playButton;
+    public GameObject textMesh;
    
     public void ActivateButton() 
     {
         playButton.SetActive(true);
+        textMesh.SetActive(true);
     }
 
     public void PlayChatScene() 
     {
         LevelManager.instance.LoadLevel("Chat");
+    }
+
+    public void ChangeWeekTest() 
+    {
+        textMesh.GetComponent<TextMeshProUGUI>().text = "Week " + CurrentLevel.indexLevel.ToString();
     }
 }
